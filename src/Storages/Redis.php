1a6228356221c8de712208bffa010b02b9da95c9
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-04-09 19:29:31 +0800

    implement Redis-based cache storage implementation using Predis library

---
+++ src/Storages/Redis.php
@@ -0,0 +1,59 @@
+<?php
+namespace Juhara\ZzzCache\Storages;
+
+use Juhara\ZzzCache\Contracts\CacheStorageInterface;
+use Predis\Client;
+
+/**
+ * Redis storage implementation using Predis library
+ *
+ * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+ */
+final class Redis implements CacheStorageInterface
+{
+    /**
+     * predis client instance
+     * @var Predis\Client
+     */
+    private $redisClient;
+
+    /**
+     * constructor
+     * @param Client $redisClient predis client instance
+     */
+    public function __construct(Client $redisClient)
+    {
+        $this->redisClient = $redisClient;
+    }
+
+    /**
+     * test availability of cache
+     * @param  string $cacheId cache identifier
+     * @return boolean true if available or false otherwise
+     */
+    public function exists($cacheId)
+    {
+        return $this->redisClient->exists($cacheId);
+    }
+
+    /**
+     * read data from storage by cache name
+     * @param  string $cacheId cache identifier
+     * @return string data from storage in serialized format
+     */
+    public function read($cacheId)
+    {
+        return $this->redisClient->get($cacheId);
+    }
+
+    /**
+     * write data to storage by cache name
+     * @param  string $cacheId cache identifier
+     * @param  string $data item to cache in serialized format
+     * @return int number of bytes written
+     */
+    public function write($cacheId, $data)
+    {
+        return $this->redisClient->set($cacheId, $data);
+    }
+}
